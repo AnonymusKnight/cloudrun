eb334bab8b0098d63978c0f6542c76397cdd8cfc
anonymusknight <smitvidhate123@gmail.com>
2023-08-04 14:41:58 +0530

    second commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("<h1> Welcome to the SMIPRI Website velociter solution</h1>");
+  response.end("<h1> Welcome to velociter solution </h1>");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
